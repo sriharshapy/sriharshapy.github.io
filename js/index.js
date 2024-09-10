c70045df51c76c19cbd1063a87d4adad8d26b258
Sri harsha Patallapalli <patallapalli.s@northeastern.edu>
2024-09-10 14:10:37 -0400

    adj

--- js/index.js
+++ js/index.js
@@ -1,10 +1,10 @@
 
 $(document).ready(function() {
-    $('.publication-mousecell').mouseover(function() {
+    $('.project-mousecell').mouseover(function() {
         $(this).find('video').css('display', 'inline-block');
         $(this).find('img').css('display', 'none');
     });
-    $('.publication-mousecell').mouseout(function() {
+    $('.project-mousecell').mouseout(function() {
         $(this).find('video').css('display', 'none');
         $(this).find('img').css('display', 'inline-block');
     });
